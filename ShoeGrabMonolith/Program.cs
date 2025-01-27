386c5058fd22d42917b545f019a45d9fc1c6b1b5
Filip Dumanowski <filip.dumanowski@qiagen.com>
2025-01-27 16:26:22 +0100

    002: add authentication and authorization, fix secret key length

--- ShoeGrabMonolith/Program.cs
+++ ShoeGrabMonolith/Program.cs
@@ -1,35 +1,71 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using ShoeGrabMonolith.Database.Contexts;
 using ShoeGrabMonolith.Extensions;
 using ShoeGrabMonolith.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContextPool<UserContext>(opt =>
-    opt.UseNpgsql(
-        builder.Configuration.GetConnectionString("PostgreSQL"),
-        o => o
-            .SetPostgresVersion(17, 0)));
+  opt.UseNpgsql(
+    builder.Configuration.GetConnectionString("PostgreSQL"),
+    o => o
+      .SetPostgresVersion(17, 0)));
 builder.Services.AddScoped<IUserManagementService, UserManagementService>();
+builder.Services.AddTransient<ITokenService, JWTAuthenticationService>();
+builder.Services.AddAuthorization();
+builder.AddJWTAuthentication();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+
+    // 1. Define the Security Scheme
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer" // Important: Must be "bearer"
+    });
+
+    // 2. Add a requirement to use the Security Scheme
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+  {
+    {
+      new OpenApiSecurityScheme
+      {
+        Reference = new OpenApiReference
+        {
+          Type = ReferenceType.SecurityScheme,
+          Id = "Bearer"
+        }
+      },
+      new string[] {}
+    }
+  });
+});
 
 var app = builder.Build();
 
 app.ApplyMigrations();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseHttpsRedirection();
 
 app.MapControllers();
 
